b6f495e380e0fdc691d0c3bff6adfd4c44876639
Celine <cmdev@outlook.fr>
2026-03-08 18:08:47 +0100

    Add like function

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -196,94 +196,93 @@ export async function deleteSauce(req, res, next) {
 /* Like or dislike a sauce */
 export function likeSauce(req, res, next) {
   try {
-    // const likedSauce = { ...req.body };
-    // client.json
-    //   .get(`sauce:${req.params.id}`)
-    //   .then((sauce) => {
-    //     let knownLike;
-    //     for (let userId of sauce.usersLiked) {
-    //       if (userId === req.authentification.userId) {
-    //         knownLike = true;
-    //       }
-    //     }
+    const likedSauce = { ...req.body };
+    client.json
+      .get(`sauce:${req.params.id}`)
+      .then((sauce) => {
+        let knownLike;
+        for (let userId of sauce.usersLiked) {
+          if (userId === req.authentification.userId) {
+            knownLike = true;
+          }
+        }
 
-    //     let knownDislike;
-    //     for (let userId of sauce.usersDisliked) {
-    //       if (userId === req.authentification.userId) {
-    //         knownDislike = true;
-    //       }
-    //     }
-    //     switch (likedSauce.like) {
-    //       case 1:
-    //         if (!knownLike && !knownDislike) {
-    //           sauce.usersLiked.push(req.authentification.userId);
-    //           client.json
-    //             .set(`sauce:${req.params.id}`, "$", {
-    //               ...sauce,
-    //               likes: sauce.likes + 1,
-    //               usersLiked: sauce.usersLiked,
-    //             })
-    //             .then(() => {
-    //               res.status(200).json({ message: "You like sauce !" });
-    //             })
-    //             .catch((error) => {
-    //               res.status(500).json({ message: "Internal Error Server" });
-    //             });
-    //         }
-    //         break;
-    //       case -1:
-    //         if (!knownLike && !knownDislike) {
-    //           sauce.usersDisliked.push(req.authentification.userId);
-    //           client.json
-    //             .set(`sauce:${req.params.id}`, "$", {
-    //               ...sauce,
-    //               dislikes: sauce.dislikes + 1,
-    //               usersDisliked: sauce.usersDisliked,
-    //             })
-    //             .then(() => {
-    //               res.status(200).json({ message: "You don't like sauce !" });
-    //             })
-    //             .catch((error) => {
-    //               res.status(500).json({ message: "Internal Error Server" });
-    //             });
-    //         }
-    //         break;
-    //       case 0:
-    //         if (knownLike) {
-    //           for (let index in sauce.usersLiked) {
-    //             if (sauce.usersLiked[index] === req.authentification.userId) {
-    //               sauce.usersLiked.splice(index, 1);
-    //               sauce.likes = sauce.likes - 1;
-    //             }
-    //           }
-    //         }
+        let knownDislike;
+        for (let userId of sauce.usersDisliked) {
+          if (userId === req.authentification.userId) {
+            knownDislike = true;
+          }
+        }
+        switch (likedSauce.like) {
+          case 1:
+            if (!knownLike && !knownDislike) {
+              sauce.usersLiked.push(req.authentification.userId);
+              client.json
+                .set(`sauce:${req.params.id}`, "$", {
+                  ...sauce,
+                  likes: sauce.likes + 1,
+                  usersLiked: sauce.usersLiked,
+                })
+                .then(() => {
+                  res.status(200).json({ message: "You like sauce !" });
+                })
+                .catch((error) => {
+                  res.status(500).json({ message: "Internal Error Server" });
+                });
+            }
+            break;
+          case -1:
+            if (!knownLike && !knownDislike) {
+              sauce.usersDisliked.push(req.authentification.userId);
+              client.json
+                .set(`sauce:${req.params.id}`, "$", {
+                  ...sauce,
+                  dislikes: sauce.dislikes + 1,
+                  usersDisliked: sauce.usersDisliked,
+                })
+                .then(() => {
+                  res.status(200).json({ message: "You don't like sauce !" });
+                })
+                .catch((error) => {
+                  res.status(500).json({ message: "Internal Error Server" });
+                });
+            }
+            break;
+          case 0:
+            if (knownLike) {
+              for (let index in sauce.usersLiked) {
+                if (sauce.usersLiked[index] === req.authentification.userId) {
+                  sauce.usersLiked.splice(index, 1);
+                  sauce.likes = sauce.likes - 1;
+                }
+              }
+            }
 
-    //         if (knownDislike) {
-    //           for (let index in sauce.usersDisliked) {
-    //             if (
-    //               sauce.usersDisliked[index] === req.authentification.userId
-    //             ) {
-    //               sauce.usersDisliked.splice(index, 1);
-    //               sauce.dislikes = sauce.dislikes - 1;
-    //             }
-    //           }
-    //         }
+            if (knownDislike) {
+              for (let index in sauce.usersDisliked) {
+                if (
+                  sauce.usersDisliked[index] === req.authentification.userId
+                ) {
+                  sauce.usersDisliked.splice(index, 1);
+                  sauce.dislikes = sauce.dislikes - 1;
+                }
+              }
+            }
 
-    //         client.json
-    //           .set(`sauce:${req.params.id}`, "$", sauce)
-    //           .then(() => {
-    //             res.status(200).json({ message: "Do you like sauce ?" });
-    //           })
-    //           .catch((error) => {
-    //             res.status(500).json({ message: "Internal Error Server" });
-    //           });
-    //         break;
-    //     }
-    //   })
-    //   .catch((error) => {
-    //     res.status(500).json({ message: "Internal Error Server" });
-    //   });
-    res.status(400).json({ message: "Portfolio project : no data saved" });
+            client.json
+              .set(`sauce:${req.params.id}`, "$", sauce)
+              .then(() => {
+                res.status(200).json({ message: "Do you like sauce ?" });
+              })
+              .catch((error) => {
+                res.status(500).json({ message: "Internal Error Server" });
+              });
+            break;
+        }
+      })
+      .catch((error) => {
+        res.status(500).json({ message: "Internal Error Server" });
+      });
   } catch (error) {
     res.status(500).json({ message: "Internal Error Server" });
   }
