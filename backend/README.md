b50baad14eea3ead6315ec8dd79decc5561c7244
Celine <cmdev@outlook.fr>
2026-03-08 11:03:06 +0100

    Update back to acl authentication

--- backend/README.md
+++ backend/README.md
@@ -1,6 +1,5 @@
 # Backend
 
-## MongoDB
 ### .env file
 
 Create a .env file with PORT, DB_KEY, DB_NAME and RANDOM_KEY variables.
@@ -10,16 +9,6 @@ Initialize variable PORT with number of your choice.
 Create a database with MongoDB Atlas.
 Initialize variable DB_KEY with the connection string to the database.
 Initialize variable DB_NAME with string that is the name of the database.
-
-Initialize variable RANDOM_KEY with string of your choice.
-
-## Redis
-
-### .env file
-
-Create a .env file with PORT, DB_KEY, DB_NAME and RANDOM_KEY variables.
-
-Initialize variable PORT with number of your choice.
 
 Initialize variable RANDOM_KEY with string of your choice.
 
--- backend/connection.js
+++ backend/connection.js
@@ -1,15 +1,21 @@
-import { createClient } from 'redis';
+import { createClient } from "redis";
+import { config } from 'dotenv';
+config();
+
 export default async function getClient() {
-    const client = createClient();
+  const dbUrl = process.env.DB_KEY;
+  const client = createClient({
+    url: dbUrl
+  });
 
-    client.on('error', err => console.log('Redis Client Error', err));
+  client.on("error", (err) => console.log("Redis Client Error", err));
 
-    const isConnected = await client.connect();
+  const isConnected = await client.connect();
 
-    if(isConnected) {
-        console.log('Connexion à Redis réussie !');
-    } else {
-        console.log('Connexion à Redis échouée !');
-    }
-    return client;
+  if (isConnected) {
+    console.log("Connexion à Redis réussie !");
+  } else {
+    console.log("Connexion à Redis échouée !");
+  }
+  return client;
 }
